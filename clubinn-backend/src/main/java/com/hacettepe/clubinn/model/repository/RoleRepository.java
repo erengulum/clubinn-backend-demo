83179aa128ae1a99c9186f4f791bfc659573bf19
erengulum <32717530+erengulum@users.noreply.github.com>
2021-04-09 19:47:59 +0300

    Delete clubinn-backend directory

--- clubinn-backend/src/main/java/com/hacettepe/clubinn/model/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hacettepe.clubinn.model.repository;
-
-import com.hacettepe.clubinn.model.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-
-    Role findByName(String roleName);
-}
