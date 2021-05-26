e12ff65e5e517586ea9665d9fbf2941e3b2acf6c
Firat Bilge <b21727006@cs.hacettepe.edu.tr>
2021-05-26 16:08:29 +0300

    Swagger Documentation added

--- src/main/java/com/hacettepe/clubinn/model/dto/FeedbackDto.java
+++ src/main/java/com/hacettepe/clubinn/model/dto/FeedbackDto.java
@@ -1,5 +1,7 @@
 package com.hacettepe.clubinn.model.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,14 +9,19 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "Feedback Data Transfer Object")
 public class FeedbackDto {
 
+    @ApiModelProperty(value = "Feedback ID")
     private Long id;
 
+    @ApiModelProperty(value = "Feedback Owner")
     private UserDto owner;
 
+    @ApiModelProperty(value = "Feedback Type")
     private String feedbackType;
 
+    @ApiModelProperty(value = "Feedback Comment")
     private String comment;
 
 }
